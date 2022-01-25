f9ef4f2fa0a9b6526ae97404384d8c856d96c436
florian <florian.c@institutsolacroup.com>
2022-01-25 14:59:13 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+
+const items = [...document.querySelectorAll('li.faq__faq-item')]
+
+
+const clearItem = (itemDetail) => {
+    itemDetail.style.maxHeight = null
+}
+
+const activateItem = (itemDetail) => {
+    const scrollHeight = itemDetail.scrollHeight
+    itemDetail.style.maxHeight = `${scrollHeight}px`
+}
+
+const handleItemClick = (e) => {
+    const clickedItem = e.target.closest('li')
+    const itemDetail = clickedItem.querySelector('.faq-item__detail')
+    const clickedItemActive = clickedItem.classList.contains('active')
+    clickedItem.classList.toggle('active')
+    if (clickedItemActive) {
+        clearItem(itemDetail)
+    } else {
+        activateItem(itemDetail)
+    }
+};
+
+const addListenersToItems = (array, callback) => {
+    array.forEach(item => {
+        item.addEventListener('click', (e) => {
+            callback(e)
+        })
+    })
+};
+
+
+addListenersToItems(items, handleItemClick)
+
+items[0].click()
